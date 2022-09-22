bc5c0e82889ae6ebb7b7ce337e2729e50c034983
Nikhil <nikhilpariyani2301@gmail.com>
2022-09-22 18:29:09 +0530

    Added cart model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_22_122403) do
+ActiveRecord::Schema.define(version: 2022_09_22_125755) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

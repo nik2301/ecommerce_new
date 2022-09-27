3b12ec07029d8cd6e716a961e322fc4c1e75d4bf
Nikhil <nikhilpariyani2301@gmail.com>
2022-09-27 11:54:24 +0530

    Tried adding scope on association between cart and cart_items

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,7 @@
 class Cart < ApplicationRecord
   belongs_to :user
+  # has_many :cart_items, -> { order(product_id: :desc) }
+  # has_many :cart_items, -> { order(created_at: :desc) }
   has_many :cart_items
   has_many :products, through: :cart_items
 end

699257093854b85f15600252522031f174e44374
Nikhil <nikhilpariyani2301@gmail.com>
2022-09-27 11:52:27 +0530

    Tried using scope on product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   # has_many :cart_items
+  scope :price_above_30000, -> { where("price > 30000") }
+  scope :with_long_name, -> { where("LENGTH(name) > 5") }
 end

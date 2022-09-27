6b8adeb8dd03bc4095e94705f7972049bd91b628
Nikhil <nikhilpariyani2301@gmail.com>
2022-09-27 16:17:18 +0530

    Added Address Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_22_175830) do
+ActiveRecord::Schema.define(version: 2022_09_27_104609) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"

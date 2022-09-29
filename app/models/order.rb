72491bfc016987c41e40fe432dea2e7a78332fe5
Nikhil <nikhilpariyani2301@gmail.com>
2022-09-29 17:54:52 +0530

    Added validation in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items, dependent: :destroy
   has_many :products, through: :order_items
+  validates_presence_of :razorpay_payment_id, :razorpay_order_id
 
   enum status: { pending: 0, completed: 1, cancelled: 2 }
 end

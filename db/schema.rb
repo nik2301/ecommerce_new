5c54d7c9e342046689474632fd63c2c711604cbf
Nikhil <nikhilpariyani2301@gmail.com>
2022-09-29 16:16:42 +0530

    Added OrderItem Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_29_102704) do
+ActiveRecord::Schema.define(version: 2022_09_29_104522) do
 
   create_table "addresses", force: :cascade do |t|
     t.text "content"
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(version: 2022_09_29_102704) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.string "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|

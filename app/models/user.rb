15ac48ca8cc73ca74630cf8a839d559d44688e10
Nikhil <nikhilpariyani2301@gmail.com>
2022-09-29 16:32:35 +0530

    Added association for user, order and order_item

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one :cart, dependent: :destroy
   has_one :address, dependent: :destroy
+  has_many :orders
+  has_many :products, through: :orders
   devise :timeoutable, :timeout_in => 1.day
 end

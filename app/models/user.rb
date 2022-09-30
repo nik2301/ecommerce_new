486e0ea3866c4de19905fc6e0468fd2fe1211151
Nikhil <nikhilpariyani2301@gmail.com>
2022-09-30 13:06:08 +0530

    Added association of user with cart_items

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one :cart, dependent: :destroy
   has_many :cart_products, through: :cart, source: :products
+  has_many :cart_items, through: :cart
   has_one :address, dependent: :destroy
   has_many :orders
   has_many :ordered_products, through: :orders, source: :products

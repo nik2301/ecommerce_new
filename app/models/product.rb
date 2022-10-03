53e2cdca8fb2e5a8b03668152df2457040c9eaf8
Nikhil <nikhilpariyani2301@gmail.com>
2022-10-03 15:22:47 +0530

    Added validation on Product for presence

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   scope :price_above_30000, -> { where("price > 30000") }
   scope :with_long_name, -> { where("LENGTH(name) > 5") }
   has_many_attached :images, dependent: :destroy
+  validates_presence_of :name, :description, :price
 end

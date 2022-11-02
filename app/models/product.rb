edc345070f9a2ba521216eae3c0b93ff8f83fd3e
Nikhil <nikhilpariyani2301@gmail.com>
2022-11-02 16:15:31 +0530

    Added concern for reviewable - refactor

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ApplicationRecord
+  include Reviewable
   acts_as_paranoid
   has_many :cart_items, dependent: :destroy
   scope :price_above_30000, -> { where("price > 30000") }
   scope :with_long_name, -> { where("LENGTH(name) > 5") }
   has_many_attached :images, dependent: :destroy
   validates_presence_of :name, :description, :price
-  has_many :reviews, as: :reviewable
   after_create :send_create_sms_to_admin
 
   def send_create_sms_to_admin

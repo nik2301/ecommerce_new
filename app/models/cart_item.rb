e24d54fda6c338145d2cd21b7e116be3f1db0da0
Nikhil <nikhilpariyani2301@gmail.com>
2023-01-09 13:46:17 +0530

    Added fragment cache on products index to improve performance

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,12 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+  after_save :touch_products
   # default_scope { order(created_at: :desc) }
+
+  private
+
+  def touch_products
+    product.update(updated_at: DateTime.now)
+  end
 end
